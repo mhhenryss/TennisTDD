6379c48399d61b3eeddea30ce6536dc67e38920c
HenryKuo <henry.kuo0707@gmail.com>
2020-07-10 19:37:38 +0800

    add step two

--- VimPractice/VimPractice/UnitTest1.cs
+++ VimPractice/VimPractice/UnitTest1.cs
@@ -4,5 +4,51 @@ namespace VimPractice
 {
 	public class Tests
 	{
+		[SetUp]
+		public void SetUp()
+		{
+			_tennis = new Tennis();
+		}
+		private Tennis _tennis;
+
+		[Test]
+		public void Love_All()
+		{
+			ScoreShouldBe(_tennis, "Love All");
+		}
+
+		[Test]
+		public void Fifteen_Love()
+		{
+			GivenFirstPlayerScore(1);
+			ScoreShouldBe(_tennis, "Fifteen Love");
+		}
+
+		[Test]
+		public void Thirty_Love()
+		{
+			GivenFirstPlayerScore(2);
+			ScoreShouldBe(_tennis, "Thirty Love");
+		}
+
+		[Test]
+		public void Forty_Love()
+		{
+			GivenFirstPlayerScore(3);
+			ScoreShouldBe(_tennis, "Forty Love");
+		}
+
+		private void GivenFirstPlayerScore(int times)
+		{
+			for (int i = 0; i < times; i++)
+			{
+				_tennis.FirstPlayerScore();
+			}
+		}
+
+		private static void ScoreShouldBe(Tennis tennis, string expected)
+		{
+			Assert.AreEqual(expected, tennis.Score());
+		}
 	}
 }
